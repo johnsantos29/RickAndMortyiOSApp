4f65ded4e7bc899abd915147d9c3f78c3c6e9a99
John Erick Santos <johnsantosdev@gmail.com>
2023-05-15 04:56:45 +1000

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,11 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by John Erick Santos on 15/5/2023.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {}
