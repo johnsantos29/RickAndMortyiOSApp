b68dd6de0a3f4788af3221090e9cce4851dee9ac
John Erick Santos <johnsantosdev@gmail.com>
2023-05-20 15:53:26 +1000

    feat: Added RMImageLoader - handles downloading images and saving them to NSCache

---
+++ RickAndMorty/Managers/RMImageLoader.swift
@@ -0,0 +1,41 @@
+//
+//  RMImageLoader.swift
+//  RickAndMorty
+//
+//  Created by John Erick Santos on 20/5/2023.
+//
+
+import Foundation
+
+final class RMImageLoader {
+    static let shared = RMImageLoader()
+    
+    private var imageDataCache = NSCache<NSString, NSData>()
+    
+    private init() {}
+    
+    /// Get image content w URI
+    /// - Parameters:
+    ///   - url: Source url
+    ///   - completion: callback
+    public func downloadImage(_ url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        let key = url.absoluteString as NSString
+        if let data = imageDataCache.object(forKey: key) {
+            completion(.success(data as Data))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                return
+            }
+            
+            let value = data as NSData
+            self?.imageDataCache.setObject(value, forKey: key)
+            completion(.success(data))
+        }
+        task.resume()
+    }
+}
